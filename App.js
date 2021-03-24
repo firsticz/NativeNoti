21d91d51082e696e095b6703a91f7c5c17842381
First <first@thai.run>
2021-03-24 20:21:13 +0700

    u

--- App.js
+++ App.js
@@ -22,6 +22,9 @@ import { Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage'
 //import firebase from 'react-native-firebase'
 import messaging from '@react-native-firebase/messaging';
+import auth from '@react-native-firebase/auth';
+import { AuthProvider } from './src/context/firebaseContext'
+
 import PushNotification from 'react-native-push-notification';
 import {
   Header,
@@ -34,80 +37,101 @@ import NotifService from './NotifService';
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import AntIcons from 'react-native-vector-icons/AntDesign'
 import Login from './src/screens/login'
+import Home from './src/screens/home'
 import NotificationList from './src/screens/notificationList'
-const notif = new NotifService();
+import Setting from './src/screens/setting'
+let notif
 const Stack = createStackNavigator()
 const Tab = createBottomTabNavigator()
-
-const user = null
-
-function MyStack(){
-  return (
-    <Stack.Navigator
-      initialRouteName="login"
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#4db6ac'
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold'
-        }
-      }}
-    >
-      {
-        user === null ? (
-          <Stack.Screen 
-            name="login"
-            component={Login}
-            options={{ title: 'Sign in', headerBackAccessibilityLabel: null}}
-            
-          />
-        ) : (
-          <Stack.Screen 
-            name="home"
-            component={Home}
-            options={{ title: 'Home'}}
-            
-          />
-        )
-      }
-      
-      
-
-    </Stack.Navigator>
-  )
-}
-
-function MyTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Login} />
-      <Tab.Screen name="notification" component={NotificationList} />
-      <Tab.Screen name="Settings" component={Login} />
-    </Tab.Navigator>
-  );
-}
-
-
-
 
 
 const App = ({ navigation }) => {
   const [loading, setLoading] = useState(true);
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
 
-  async function requestUserPermission() {
-    const authStatus = await messaging().requestPermission();
-    const enabled =
-      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  function MyStack(){
+    return (
+      <Stack.Navigator
+        initialRouteName="login"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#4db6ac'
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold'
+          }
+        }}
+      >
+        {
+          !user ? (
+            <Stack.Screen 
+              name="login"
+              component={Login}
+              options={{ title: 'Sign in', headerBackAccessibilityLabel: null}}
+              
+            />
+          ) : (
+            <Stack.Screen 
+              name="home"
+              component={Home}
+              options={{ title: 'Home'}}
+              
+            />
+          )
+        }
+      </Stack.Navigator>
+    )
+  }
   
-    if (enabled) {
-      console.log('Authorization status:', authStatus);
-    }
+  function MyTabs() {
+    return (
+      <Tab.Navigator>
+        <Tab.Screen 
+          name="home" 
+          component={Home} 
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="home" />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="notification" 
+          component={NotificationList}
+          options={{
+            tabBarLabel: 'Notification',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="notifications-active" />
+            ),
+          }}
+         />
+        <Tab.Screen
+        name="Settings"
+        component={Setting}
+        options={{
+          tabBarLabel: 'setting',
+          tabBarIcon: ({ color, size }) => (
+            <AntIcons name="setting" />
+          ),
+        }}
+        />
+      </Tab.Navigator>
+    );
   }
 
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+  
 
   async function registerAppWithFCM() {
     await messaging().registerDeviceForRemoteMessages();
@@ -128,6 +152,7 @@ const App = ({ navigation }) => {
       const fcmToken = await messaging().getToken();
       console.log('end granted');
       console.log(fcmToken);
+      notif = new NotifService(fcmToken);
       console.log('User granted messaging permissions!');
     } else {
       console.log('User declined messaging permissions :(');
@@ -164,19 +189,29 @@ const App = ({ navigation }) => {
           );
         }
       });
+    
 
     const unsubscribe = messaging().onMessage(async remoteMessage => {
-      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
-      notif.localNotif(remoteMessage.title, remoteMessage.body )
+      //Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+      const Message =  JSON.parse(JSON.stringify(remoteMessage));
+      notif.localNotif(Message.notification.title, Message.notification.body )
 
       // PushNotification.localNotification({
-      //   title: 'notification.title',
-      //   message: 'notification.body!',
+      //   title: Message.notification.title,
+      //   message:  Message.notification.body,
       // });
     });
 
     return unsubscribe;
   }, []);
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+  
+
+  if (initializing) return null
   
   // if (loading) {
   //   return null;
@@ -188,9 +223,12 @@ const App = ({ navigation }) => {
 
 
   return (
-    <NavigationContainer>
-      {!user? <MyTabs /> : <MyStack />}
-    </NavigationContainer>
+    <AuthProvider>
+      <NavigationContainer>
+        {user? <MyTabs /> : <MyStack />}
+      </NavigationContainer>
+    </AuthProvider>
+    
   );
 }
 
